2d23f78126ee90b6acf47ec2f669797854a5793b
okunoyuto0000 <okunoyuto0000@gmail.com>
2021-03-14 18:00:27 +0000

    [Add] 3-8 編集画面機能

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   get 'books/new'
   post 'books' => 'books#create'
   get 'books/:id' => 'books#show', as: 'book'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  patch 'books/:id' => 'books#update', as: 'update_book'
 end
