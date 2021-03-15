f5f3da3a7f7e473196518ae027aa7bc73565d033
okunoyuto0000 <okunoyuto0000@gmail.com>
2021-03-15 09:51:45 +0000

    [Fix] 3-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -36,7 +36,7 @@ class BooksController < ApplicationController
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to '/books/index'
+    redirect_to '/books'
   end
 
   private
