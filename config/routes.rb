4d51c2f13990712edff9f4ad937ce376b2546400
okunoyuto0000 <okunoyuto0000@gmail.com>
2021-03-15 09:42:03 +0000

    [Finish] 3-8

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'homes/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homes#index'
-  get 'books/index'
+  get 'books' => 'books#index'
   post 'books' => 'books#create'
   get 'books/:id' => 'books#show', as: 'book'
   get 'books/:id/edit' => 'books#edit', as: 'edit_book'
